67549ff24bdbd9a4a63e420fcb52fc530bdfde03
rebeccax06 <102809822+rebeccax06@users.noreply.github.com>
2024-04-30 12:57:50 -0400

    ferb

--- world.gd
+++ world.gd
@@ -1,1 +1,11 @@
 extends Node2D
+
+@onready var collision_polygon_2d = $StaticBody2D/CollisionPolygon2D
+@onready var polygon_2d = $StaticBody2D/CollisionPolygon2D/Polygon2D
+#called when the node enters the scene tree for the first time
+func _ready():
+	polygon_2d.polygon = collision_polygon_2d.polygon
+
+#called every frame. 'delta' is the elapsed time since the previous frame
+func _process(delta):
+	pass
