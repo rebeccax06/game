345c5f22d78807ee7b3e517bac6d6d11b0705e1a
rebeccax06 <102809822+rebeccax06@users.noreply.github.com>
2024-05-08 12:18:33 -0400

    speed

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends CharacterBody2D
 
 #stores speed and jump velocity
-const SPEED = 100.0
+const SPEED = 150.0
 const JUMP_VELOCITY = -300.0
 const ACCELERATION = 600.0
 const FRICTION = 1000.0
